e76c3628119cb9340fc97cba529e81e1d28062c6
Veasba01 <mauricioarcez01@gmail.com>
2025-07-04 00:11:10 -0600

    configuracion del SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://uniwarecr.com',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        },
+      },
+    },
+  },
+});
